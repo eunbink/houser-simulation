7d38330098b46a03e408edc84e50e735180ef92d
Taylor Sam <taylorsam209@yahoo.com>
2017-09-29 15:44:46 -0600

    wizard3 created

--- sim2/src/components/wizardThree/WizardThree.js
+++ sim2/src/components/wizardThree/WizardThree.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-
-
-
+import Header from '../header/Header';
+import './WizardThree.css';
+import { Link } from 'react-router-dom';
+import stepActive from '../../assets/step_active.png';
+import stepCompleted from '../../assets/step_completed.png';
+import stepInactive from '../../assets/step_inactive.png';
+import axios from 'axios';
+import { connect } from 'react-router';
 
 class WizardThree extends Component {
-    constructor(){
-        super();
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className='main_container'>
+                    <div className='content_container'>
+                        <div className='addListing'>
+                            <h3>Add new listing</h3>
+                            <Link to='/dashboard'><button>Cancel</button></Link>
+                        </div>
+                        <div className='step'>
+                            <span>Step 3</span>
+                            <div className='what_stage'>
+                            <img src={stepCompleted} />
+                            <img src={stepCompleted}/>
+                            <img src={stepActive}/>
+                            <img src={stepInactive}/>
+                            <img src={stepInactive}/>
+                            </div>
+                        </div>
+                        <div className= 'imgContainer'>
+                            <p>Preview</p>
+                            </div>
 
 
+                        <Link to='/wizard/2'><button>Next Step</button></Link>
+                    </div>
+
+                </div>
+            </div>
+        )
     }
 }
 
