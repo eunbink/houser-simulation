d5cb2e01ecdddb57b0460d88581c1c425927b8a7
Mikel North <mikel.north@gmail.com>
2017-09-29 14:04:47 -0600

    header

--- sim2/src/components/header/Header.js
+++ sim2/src/components/header/Header.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './Header.css'
-
 import logo from '../../assets/header_logo.png'
-
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
-    constructor() {
-        super();
-
-    }
 
     render() {
         return (
             <div className='header'>
                 <div className='header_child'>
-                    <img className='header_logo' src={logo}/>
+                    
+                    <div className='header_left'>
+                    <img className='header_logo' src={logo} />
+                        <h3>Houser</h3><h3>Dashboard</h3>
+                    </div>
+                    <Link to='/' className='logout'><span>Logout</span></Link>
+                    
                 </div>
             </div>
         )
