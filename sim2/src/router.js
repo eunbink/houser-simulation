ca141c80d6927affb596e2135452db67c31bdf93
Taylor Sam <taylorsam209@yahoo.com>
2017-09-29 12:22:14 -0600

    Created Login Component

--- sim2/src/router.js
+++ sim2/src/router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from './components/dashboard/Dashboard.js'
-// import Login from './components/login/Login.js'
+import Login from './components/login/Login.js'
 import WizardOne from './components/wizardOne/WizardOne'
 import WizardTwo from './components/wizardTwo/WizardTwo.js'
 import WizardThree from './components/wizardThree/WizardThree.js'
@@ -13,7 +13,7 @@ export default (
     <div>
     
     <Switch>
-        {/* <Route component={ Login } exact path='/'/> */}
+        <Route component={ Login } exact path='/'/>
         <Route component={ Dashboard } path='/dashboard'/>
         <Route component ={ WizardOne } path='/wizard/1'/>
         <Route component ={ WizardTwo } path='/wizard/2'/>
