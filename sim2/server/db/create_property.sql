1d1be5463bdbd7a99218aa77fd478292d3383811
Ross Dunford <dunford@Rus-MacBook.local>
2017-09-29 12:23:11 -0600

    0.0.1

---
+++ sim2/server/db/create_property.sql
@@ -0,0 +1,1 @@
+INSERT INTO Property (propertyName, propertyDesrcription, address, city, state, zip, imgURL, loanAmount, monthlyMortgage, desiredRent) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 );
