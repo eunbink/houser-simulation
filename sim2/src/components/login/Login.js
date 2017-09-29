b59787dad3ad4e3e72f56190a3c82d49c401be33
Taylor Sam <taylorsam209@yahoo.com>
2017-09-29 13:19:10 -0600

    Styled Login

--- sim2/src/components/login/Login.js
+++ sim2/src/components/login/Login.js
@@ -8,29 +8,15 @@ class Login extends Component {
     render() {
         return (
             <div className="login">
-                {/* <div className = "loginContainer"> */}
-                    <div className='logocontainer'>
-                        <img src={authLogo} className="App-logo" alt="logo" />
-                    </div>
-                    <div className="Shelflogo">
-                        <h1>SHELFIE</h1>
-                    </div>
-                <div className='Item_content'>
-                    {/* <div className='Left_content'>
-                        <img src={logo2} className="item-photo" alt='item' />
-                    </div> */}
-                    <div className="Right_content">
+                <div className = "loginContainer">
+                        <img src={authLogo} className="authLogo" alt="logo" />
                         <p className='itemtext'>Name</p>
                         <input className='item_input' type='text' />
                         <p className='itemtext'>Price</p>
                         <input className='item_input' type='text' />
-                        <div>
                             <button>Edit</button>
                             <button>Delete</button>
-                        </div>
                     </div>
-                  </div>
-                {/* </div> */}
             </div>
         );
     }
