da83f382a17de24f87dd83f3aa7fe60803eef7dc
Anthony McWhorter <anthonymcwhorter@Anthonys-MacBook-Pro.local>
2017-09-29 15:53:10 -0600

    Logan 2

--- sim2/src/components/login/Login.js
+++ sim2/src/components/login/Login.js
@@ -1,27 +1,59 @@
-import React, { Component } from 'react';
-import authLogo from '../../assets/auth_logo.png';
-import './Login.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import authLogo from "../../assets/auth_logo.png";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { connect } from "react-redux";
+import { updateUserName, updatePassword } from "../../ducks/reducer";
 
 class Login extends Component {
-
-    render() {
-        return (
-            <div className="login">
-                <div className = "loginContainer">
-                        <img src={authLogo} className="authLogo" alt="logo" />
-                        <span className='itemtext'>Username</span>
-                        <input className='item_input' type='text' />
-                        <span className='itemtext'>Password</span>
-                        <input className='item_input' type='text' />
-                        <div className='buttonContainer'>
-                           <Link to='/dashboard'> <button className = "loginbutton">Login</button></Link>
-                            <Link to='/dashboard'><button className= "registerButton">Register</button></Link>
-                            </div>
-                    </div>
-            </div>
-        );
-    }
+  render() {
+    const { updateUserName, updatePassword } = this.props;
+    return (
+      <div className="login">
+        <div className="loginContainer">
+          <img src={authLogo} className="authLogo" alt="logo" />
+          <span className="itemtext">Username</span>
+          <input
+            className="item_input"
+            type="text"
+            onChange={e => {
+              updateUserName(e.target.value);
+            }}
+          />
+          <span className="itemtext">Password</span>
+          <input
+            className="item_input"
+            type="text"
+            onChange={e => {
+              updatePassword(e.target.value);
+            }}
+          />
+          <div className="buttonContainer">
+            <Link to="/dashboard">
+              {" "}
+              <button className="loginbutton" onClick={handle}>
+                Login
+              </button>
+            </Link>
+            <Link to="/dashboard">
+              <button className="registerButton">Register</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login;
+function mapStateToProps(state) {
+  const { updateUserName, updatePassword } = state;
+  return {
+    userName,
+    password
+  };
+}
+
+export default connect(mapStateToProps, { updateUserName, updatePassword })(
+  Login
+);
