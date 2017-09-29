0a5a766a12ec563feaac2495efaa82e02d220421
Ross Dunford <dunford@Rus-MacBook.local>
2017-09-29 15:52:29 -0600

    reducer

--- sim2/src/ducks/reducer.js
+++ sim2/src/ducks/reducer.js
@@ -9,7 +9,8 @@ const initialState = {
     loanAmount: 0,
     monthlyMortgage: 0,
     desiredRent: 0,
-    userId: 0
+    userId: 0,
+    password: ''
 }
 
 const UPDATE_PROPERTY_NAME = 'UPDATE_PROPERTY_NAME';
@@ -23,6 +24,7 @@ const UPDATE_LOAN_AMOUNT = 'UPDATE_LOAN_AMOUNT';
 const UPDATE_MONTHLY_MORTGAGE = 'UPDATE_MONTHLY_MORTGAGE';
 const UPDATE_DESIRED_RENT = 'UPDATE_DESIRED_RENT';
 const UPDATE_USERID = 'UPDATE_USERID';
+const UPDATE_PASSWORD = 'UPDATE_PASSWORD';
 
 function reducer(state = initialState, action) {
     switch (action.type) {
@@ -46,27 +48,97 @@ function reducer(state = initialState, action) {
             return Object.assign({}, state, { monthlyMortgage: action.payload });
         case UPDATE_DESIRED_RENT:
             return Object.assign({}, state, { desiredRent: action.payload });
+        case UPDATE_USERID:
+            return Object.assign({}, state, { userId: action.payload });
+        case UPDATE_PASSWORD:
+            return Object.assign({}, state, { password: action.payload });
         default:
             return state;
     }
 }
 
-    export function updatePropertyName(propertyName) {
-        return {
-            type: UPDATE_PROPERTY_NAME,
-            payload: propertyName
-        }
+export function updatePropertyName(propertyName) {
+    return {
+        type: UPDATE_PROPERTY_NAME,
+        payload: propertyName
     }
-    export function updatePropertyDescription(propertyDescription) {
-        return {
-            type: UPDATE_PROPERTY_DESCRIPTION,
-            payload: propertyDescritpion
-        }
+}
+export function updatePropertyDescription(propertyDescription) {
+    return {
+        type: UPDATE_PROPERTY_DESCRIPTION,
+        payload: propertyDescritpion
     }
+}
 
-    export function updateAddress(address) {
-        return {
-            type: UPDATE_ADDRESS,
-            payload: address
-        }
+export function updateAddress(address) {
+    return {
+        type: UPDATE_ADDRESS,
+        payload: address
     }
+}
+
+export function updateState(state) {
+    return {
+        type: UPDATE_STATE,
+        payload: state
+
+    }
+}
+
+export function updateCity(city) {
+    return {
+        type: UPDATE_CITY,
+        payload: city
+    }
+}
+
+export function updateZip(zip) {
+    return {
+        type: UPDATE_ZIP,
+        payload: zip
+    }
+}
+
+export function updateIMGURL(imgurl) {
+    return {
+        type: UPDATE_IMGURL,
+        payload: imgurl
+    }
+}
+
+export function updateLoanAmount(loanAmount) {
+    return {
+        type: UPDATE_LOAN_AMOUNT,
+        payload: loanAmount
+    }
+}
+
+export function updateMonthlyMortgage(monthlyMortgage) {
+    return {
+        type: UPDATE_MONTHLY_MORTGAGE,
+        payload: monthlyMortgage
+    }
+}
+
+export function updateDesiredRent(desiredRent) {
+    return {
+        type: UPDATE_DESIRED_RENT,
+        payload: desiredRent
+    }
+}
+
+export function updateUserId(userId) {
+    return {
+        type: UPDATE_USERID,
+        payload: userId
+    }
+}
+
+export function updatePassword(password){
+    return {
+        type: UPDATE_PASSWORD,
+        payload: password
+    }
+}
+
+export default reducer;
